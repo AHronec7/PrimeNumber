f34cb1de2eb9bbf8df9635e4c8335876679dbbfc
AHronec7 <austinhronec16@yahoo.com>
2018-03-26 17:24:27 -0400

    Initial Upload

---
+++ PrimeNumberGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+
+
+
+                bool isPrime = true;
+                Console.WriteLine("Prime Numbers : ");
+                for (int i = 3; i > 0; i++)
+                {
+                    for (int j = 3; j <= 800000; j++)
+                    {
+
+                        if (i != j && i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine("\t" + i);
+                    }
+                    isPrime = true;
+                }
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
